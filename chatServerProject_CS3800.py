35a37cd2874f015f83803a47ea2f3b15fcd37fd6
Nick.L <65532770+1992q1020@users.noreply.github.com>
2022-10-19 10:33:42 -0700

    Add files via upload

---
+++ chatServerProject_CS3800.py
@@ -0,0 +1,28 @@
+﻿#server.py
+import socket
+ 
+# socket server
+ip = ''  
+port = 8888
+socket_server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+# 2.ip,port
+socket_server.bind((ip, port))
+# 3.start listening
+socket_server.listen(1)  # block when more than one connection, allowed only one connection
+while True:
+    conn, addr = socket_server.accept()  # wait untill there is new connection
+    with conn:
+        print("connect by", addr)
+        try:
+            while True:
+                data = conn.recv(1024)
+                print("server recevie peername and data:", conn.getpeername(), data.decode())
+                if data:
+                    # conn.sendall(data)  # retutrn all data to client
+                    response = input(">>>")
+                    conn.sendall(response.encode())
+                    print("send to client:", response)
+                else:
+                    break
+        except ConnectionResetError as e:
+            print("Disconnected ")
